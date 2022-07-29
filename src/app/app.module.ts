1812a17d27ea3cdbf25850be0ef0807c20c4e4f4
Yasmin Benicio <79879086+yasxxv@users.noreply.github.com>
2022-07-29 01:20:22 -0300

    Add files via upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,40 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { CourseListComponent } from './courses/course-list.component';
+import { ReplacePipe } from './pipe/replace.pipe';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { Error404Compoennt } from './error-404/erro-404.compoennt';
+import { CourseInfoComponent } from './courses/course-info.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CourseListComponent,
+    ReplacePipe,
+    NavBarComponent,
+    Error404Compoennt,
+    CourseInfoComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: 'courses', component: CourseListComponent
+      }, {
+        path: 'courses/info/:id', component: CourseInfoComponent
+      }, {
+        path: '', redirectTo: 'courses', pathMatch: 'full'
+      }, {
+        path: '**', component: Error404Compoennt
+      }
+    ]),
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
